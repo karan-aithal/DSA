0c3dfb267e3f2963fcdf228aa84e76afaa469902
karan-aithal <karanaithal841@gmail.com>
2025-08-19 17:31:40 +0530

    Buy low, sell high

--- Arrays/StockBuySell.cpp
+++ Arrays/StockBuySell.cpp
@@ -7,21 +7,51 @@
 #include <set>
 using namespace std;
 
-void solve() {
-    
+// Buy stock at lowest price, sell at highest ,i <= j , i.e j = i+1
+// Can be solved using Kadane's Algorithm -- chek
+int StockBS(int arr[], int n)
+{
+
+    int minPrice = INT_MAX;
+    int maxProf = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        // Update the minPrice if it is greater than the current element of the array
+        minPrice = min(arr[i], minPrice);
+
+        // difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
+        // Max Difference -- any diff
+        // stock - diff with small element first 
+        maxProf = max(arr[i] - minPrice, maxProf);
+    }
+
+    return maxProf;
 }
 
-int main() {
+int main()
+{
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    int t;     cin >> t;
+    int t;
+    cin >> t;
     while (t--)
     {
+
+        int n = 0;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
         // Input for array ,number etc...
 
-        solve();
-        // Call solve or simillar function with args
+        int res = StockBS(arr, n);
+
+        cout << "The maximum profit is " << res;
 
         return 0;
     }
