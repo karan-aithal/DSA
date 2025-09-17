9bc384d12588b3d7fc106f9bccbeed3e649f9d59
karan-aithal <karanaithal841@gmail.com>
2025-09-17 23:52:04 +0530

    sortedSquaresi in array

--- BasicMath/SquaresArray.cpp
+++ BasicMath/SquaresArray.cpp
@@ -7,17 +7,42 @@
 #include <set>
 using namespace std;
 
-void sortedSquares(vector<int>& nums) {
+vector<int> sortedSquares(vector<int> &nums)
+{
 
-    for (int i = 0; i < nums.size(); i++)
+    int LP = 0;
+    int RP = nums.size() - 1;
+    vector<int> res(nums.size(), 0);
+    // three pointer
+
+    for (int i = nums.size()-1; i > 0; i--) // place from the end asscending order
     {
-        nums[i] = nums[i] * nums[i];
+        if (nums[LP] * nums[LP] > nums[RP] * nums[RP])
+        {
+            res[i] = nums[LP] * nums[LP];
+            LP++;
+        }
+        else
+        {
+            res[i] = nums[RP] * nums[RP];
+            RP--;
+        }
     }
-    
- // for negative nums sorting is difficult ?? 
-    // HOW to sort ??? 
 
+    return res;
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     nums[i] = nums[i] * nums[i];
+    // }
 
+    // for negative nums in a sorted array we need to sort again !!
+
+    //  nums = {-4, -1 , 0 , 2 , 8 , 9}
+    //  res = {16, 1, 0, 4, 64, 81}  ❌ Not Sorted
+    //  res =  {0 ,1 ,4 , 16, 64, 81}  ✔ Correct
+
+    // HOW to sort ???
+    // Two pointer start and end
 }
 
 int main()
@@ -33,11 +58,11 @@ int main()
 
     for (int i = 0; i < n; i++)
         cin >> nums[i];
-    
-    //int k ; cin << k;
-    // int x;
-    // cin >> x;
-    sortedSquares( nums);
+
+    // int k ; cin << k;
+    //  int x;
+    //  cin >> x;
+    sortedSquares(nums);
     // SearchElement(arr, size, x);
 
     // cout << "The array after removing duplicate elements is " << endl;
